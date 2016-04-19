0258ccbcee34413e7212072a0a6e1f2136392156
Joscha Rohmann <joscha@kanaye.net>
2016-04-20 00:23:03 +0200

    First steps to make it a proxy adapter.

--- loki-msgpack-adapter.js
+++ loki-msgpack-adapter.js
@@ -1,58 +1,71 @@
 /**
  * @Author Joscha Rohmann <joscha@kanaye.net>
  * @License MIT
- * A msgpack file adapter for LokiJS using msgpack-lite. 
+ * A "proxy" persistence adapter for LokiJS using msgpack-lite for messagepack encoding.
  */
+(function (root, factory) {
+        if (typeof define === 'function' && define.amd) {
+                define(['msgpack-lite', 'lokijs'],factory);
+        } else if (typeof exports == 'object') {
+                module.exports = factory(require('msgpack-lite'), require('lokijs'));
+        } else {
+                root.lokiMsgpackAdapter = factory(root.msgpack, root.loki);
+        }
+})(this, function (msgpack, loki) {
+        if (!msgpack || !loki) {
+                throw new Error('You have to include lokijs and msgpack-lite for this adapter to work');
+        }
 
-/* dependencies */
-var fs = require('fs');
-var msgpack = require('msgpack-lite');
+        /**
+        * constructor for the apapter
+        * @param {string} filename the filename/path of the file to store the database
+        * @param {object} options  Options object for (currently) adapter compatibility.
+        */
+        function lokiMsgpackAdapter (filename, options) {
+              if (typeof filename == 'object') {
+                  this.options = filename;
+              } else {
+                  this.options = options || {};
+              }
+        }
 
-/** 
-* constructor for the apapter
-* @param {string} filename the filename/path of the file to store the database
-*/
-function lokiMsgpackAdapter (filename) {
-    this.filename = filename;
-}
-/* using reference as we're seializing in msgpack not json */
-lokiMsgpackAdapter.prototype.mode = 'reference';
+        /* using reference as we're seializing in msgpack not json */
+        lokiMsgpackAdapter.prototype.mode = 'reference';
 
-/**
-* saves / exports the database
-* @param {string} dbname Will be used as the filename if constructors filename argument is not provided.
-* @param {object} dbref Reference to the Loki instance.
-* @param {function} fn Callback function.
-*/
-lokiMsgpackAdapter.prototype.exportDatabase = function (dbname, dbref, fn) {
-    var binary;
+        /**
+        * saves / exports the database
+        * @param {string} dbname Will be used as the filename if constructors filename argument is not provided.
+        * @param {object} dbref Reference to the Loki instance.
+        * @param {function} fn Callback function.
+        */
+        lokiMsgpackAdapter.prototype.exportDatabase = function (dbname, dbref, fn) {
+              var binary;
 
-    try {
-        binary = msgpack.encode(dbref);
-    } catch (e) {
-        return fn(e);
-    }
+              try {
+                  binary = msgpack.encode(dbref);
+              } catch (e) {
+                  return fn(e);
+              }
+              this.persistenceAdapter.saveDatabase(dbname, binary, fn);
+        };
 
-    fs.writeFile(this.filename || dbname, binary, fn);
-};
-/**
- * Loads the database from an msgpack file.
- * @param  {string}   dbname Will be used as the filename if the constructors filename argument is not provided.
- * @param  {Function} fn     [description]
- */
-lokiMsgpackAdapter.prototype.loadDatabase = function (dbname, fn) {
-    var binary;
-    try {
-    	binary = fs.readFileSync(this.filename || dbname);
-    } catch(e) {
-    	return fn(e);
-    }
+        /**
+         * Loads the database from an msgpack file.
+         * @param  {string}   dbname Will be used as the filename if the constructors filename argument is not provided.
+         * @param  {Function} fn     [description]
+         */
+        lokiMsgpackAdapter.prototype.loadDatabase = function (dbname, fn) {
+                this.persistenceAdapter.loadDatabase(dbname, function (err, binary) {
+                        if (err) {
+                                return fn(err, null);
+                        }
 
-    try {
-    	fn( msgpack.decode(binary));
-    } catch(e) {
-    	fn(e);
-    }
-};
-
-module.exports = lokiMsgpackAdapter;
+                       try {
+                               fn(null, msgpack.decde(binary));
+                       } catch (err) {
+                                fn(err, null);
+                       }
+                });
+        };
+        module.exports = lokiMsgpackAdapter;
+});
